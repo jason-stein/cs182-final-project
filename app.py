aaa339ab071e8a827417e5c5d00722cc15dde1a7
Jason Stein <jason.am.stein@gmail.com>
2016-11-29 16:56:59 -0500

    mostly just comments tbh

--- app.py
+++ app.py
@@ -5,19 +5,13 @@ import sys
 import matplotlib.pyplot as plt
 
 n = 25
-
 graph = generate.SocialGraph()
-
+plt.figure(1)
 graph.plotStudents('ro')
-
-graph.train(3000)
-
-graph.plotStudents('bs')
 
 start = time.time()
 avg1 = 0.0
 n1 = n
-
 for i in xrange(n):
 	# separation = len(search.BFS(random.choice(graph.students),random.choice(graph.students),graph))
 	separation = len(search.BFS(graph.students[i],graph.students[i+1],graph))
@@ -32,7 +26,6 @@ for i in xrange(n):
 avg1 = avg1 / n1
 end = time.time()
 diff1 = end - start
-
 print ""
 
 start = time.time()
@@ -45,15 +38,37 @@ for i in xrange(n):
 	else:
 		n2 -= 1
 	progress = '|' + '#' * int(float(i+1) / float(n) * 10) + ' '  * (10 - int(float(i+1) / float(n) * 10)) + '|'
-	sys.stdout.write("aStar iter " + str(i+1) + ' ' + progress + "\r")
+	sys.stdout.write("aStar untrained iter " + str(i+1) + ' ' + progress + "\r")
 	sys.stdout.flush()
 avg2 = avg2 / n2
 end = time.time()
 diff2 = end - start
+print ""
 
+
+graph.train(400000)
+plt.figure(2)
+graph.plotStudents('gs')
+
+start = time.time()
+avg3 = 0.0
+n3 = n
+for i in xrange(n):
+	separation = len(search.aStar(graph.students[i],graph.students[i+1],graph, ))
+	if separation != 0:
+		avg3 += separation
+	else:
+		n3 -= 1
+	progress = '|' + '#' * int(float(i+1) / float(n) * 10) + ' '  * (10 - int(float(i+1) / float(n) * 10)) + '|'
+	sys.stdout.write("aStar trained iter " + str(i+1) + ' ' + progress + "\r")
+	sys.stdout.flush()
+avg3 = avg3 / n3
+end = time.time()
+diff3 = end - start
 print ""
 
 print "BFS -- Time:", diff1, " Avg:", avg1, " Iterations:", n
-print "aStar -- Time:", diff2, " Avg:", avg2, " Iterations:", n
+print "aStar untrained -- Time:", diff2, " Avg:", avg2, " Iterations:", n
+print "aStar trained -- Time:", diff3, " Avg:", avg3, " Iterations:", n
 
 plt.show()
--- generate.py
+++ generate.py
@@ -3,16 +3,20 @@
 import random
 import util
 import sys
+import math
 import matplotlib.pyplot as plt
 
-
-HOUSE_PROB_INC = .4
+# constants for probability increase on shared characteristics
+HOUSE_PROB_INC = .5
 INTEREST_PROB_INC = .1
-YEAR_PROB_INC = .1
-BASE_PROB = .05
-NSTUDENTS = 1000
+YEAR_PROB_INC = .2
+BASE_PROB = 0
+NSTUDENTS = 2000
 
+# inverse square root of 2 -- bound for heuristic grid
+ISQR2 = 1 / math.sqrt(2.0)
 
+# class for individual student -- contains attributes and location
 class Student:
 
 	def __init__(self):
@@ -22,10 +26,11 @@ class Student:
 			"Winthrop"])
 		self.interest = random.choice(["academics","arts","sports"])
 		self.year = random.choice([17,18,19,20])
-		self.pos = [random.random(), random.random()]
+		self.pos = [random.uniform(0,ISQR2), random.uniform(0,ISQR2)]
 
-def maybeFriends(s1,s2):		# randomly decides if s1, s2 friends based on 
-	p = BASE_PROB				# shared characteristics
+# randomly decides if s1, s2 friends based on shared characteristics
+def maybeFriends(s1,s2):
+	p = BASE_PROB
 	if s1.house == s2.house:
 		p += HOUSE_PROB_INC
 	if s1.interest == s2.interest:
@@ -34,33 +39,42 @@ def maybeFriends(s1,s2):		# randomly decides if s1, s2 friends based on
 		p += YEAR_PROB_INC
 	return util.flipCoin(p)
 
+# push students closer or farther depending on friendship status
 def changeDistance(student1, student2, friends):
     dx = student2.pos[0] - student1.pos[0]
     dy = student2.pos[1] - student1.pos[1]
     change = 1.0
     if friends:
-        change = 0.9
+        change = 0.5
     else:
-        change = 1.1
+        change = 1.05
     new_dx = dx * change
     new_dy = dy * change
     new_x = student1.pos[0] + new_dx
     new_y = student1.pos[1] + new_dy
-    if new_x > 1.0:
-        new_x = 1.0
+    if new_x > ISQR2:
+        new_x = ISQR2
     if new_x < 0.0:
         new_x = 0.0
-    if new_y > 1.0:
-        new_y = 1.0
+    if new_y > ISQR2:
+        new_y = ISQR2
     if new_y < 0.0:
         new_y = 0.0
 
     student2.pos = [new_x, new_y] 
 
+#
+# class for social graph. contains students, friendship adjacency matrix,
+# heuristic matrix, and heuristic training function, as well as various
+# display / debug functions
+#
 class SocialGraph:
 
+	# generates NSTUDENTS random studends, creates 
+	# adjacency and heuristic matrices
 	def __init__(self):
 
+		print "Generating data set ( n =",NSTUDENTS,")"
 		self.students = []
 		self.adjMatrix = []
 		self.heuristicMatrix = []
@@ -81,16 +95,20 @@ class SocialGraph:
 					self.adjMatrix[i][j] = self.adjMatrix[j][i] = 1
 		for i in xrange(NSTUDENTS):
 			for j in xrange(i):
-				dis = util.cartesianDistance(self.students[i].pos, self.students[j].pos)
+				dis = util.cartesianDistance(
+					self.students[i].pos, self.students[j].pos)
 				self.heuristicMatrix[i][j] = self.heuristicMatrix[j][i] = dis
 
 	def prettyPrintGraph(self):
 		for i in self.adjMatrix:
 			print i
+
 	def prettyPrintHeuristic(self):
 		for i in self.heuristicMatrix:
 			print i
 
+	# trains the data set: for each iteration, finds random pair and updates
+	# distance based on friendship status
 	def train(self, iterations):
 		for i in xrange(iterations):
 			sys.stdout.write("Training: " + str(i+1) + "\r")
@@ -107,12 +125,15 @@ class SocialGraph:
 			else:
 				# Push p2 away
 				changeDistance(s1, s2, False)
-			self.heuristicMatrix[s1.id][s2.id] = self.heuristicMatrix[s2.id][s1.id] = util.cartesianDistance(s1.pos, s2.pos)
+			self.heuristicMatrix[s1.id][s2.id] = \
+			self.heuristicMatrix[s2.id][s1.id] = \
+			util.cartesianDistance(s1.pos, s2.pos)
 		print ""
 
+	# plots each student's position as a point with customizable format string
 	def plotStudents(self,style):
 		xs = [student.pos[0] for student in self.students]
 		ys = [student.pos[1] for student in self.students]
 		plt.plot(xs,ys,style)
-		plt.axis([0,1,0,1])
+		plt.axis([0,ISQR2,0,ISQR2])
 		plt.draw()
