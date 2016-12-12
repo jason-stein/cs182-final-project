07b22e4a60ff679776dc62b326f3629089cb6815
Jason Stein <jason.am.stein@gmail.com>
2016-12-11 20:05:40 -0500

    computer science is fake

--- testscript.py
+++ testscript.py
@@ -10,9 +10,9 @@ result1 = search.BFS(x,y,graph)
 result2 = (search.IDDFS(x,y,graph),0)
 result3 = search.BFS2(x,y,graph)
 result4 = search.aStar(x,y,graph)
-print result1
-print result2
-print result3
-print result4
+print "BFS path and # nodes expanded:", result1
+print "Iter. Deepening path:", result2[0]
+print "2-sided BFS path and # nodes expanded:", result3
+print "A* path and # nodes expanded:",result4
 results = [result1,result2,result3,result4]
-print all(len(result[0]) == len(result1[0]) for result in results)
+print "All paths same length:", all(len(result[0]) == len(result1[0]) for result in results)
